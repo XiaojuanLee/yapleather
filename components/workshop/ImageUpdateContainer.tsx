f43237a9f2b0fb0a7f9a7d408f5f48f7f6ded8a9
Jean Li <hi.xiaojuan.li@gmail.com>
2024-12-10 00:17:12 -0500

    Add new features: Delete Image.

--- components/workshop/ImageUpdateContainer.tsx
+++ components/workshop/ImageUpdateContainer.tsx
@@ -7,15 +7,14 @@ import { SubmitButton } from '../form/Buttons';
 import { type actionFunction } from '@/utils/types';
 
 type ImageInputContainerProps = {
-  images: string[];
-  name: string;
+
   action: actionFunction;
   text: string;
   children?: React.ReactNode;
 };
 
 function ImageUpdateContainer(props: ImageInputContainerProps) {
-  const { images, name, action, text } = props;
+  const { action, text } = props;
   const [isUpdateFormVisible, setUpdateFormVisible] = useState(false);
 
  
